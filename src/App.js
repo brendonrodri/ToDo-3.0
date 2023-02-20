76383e7d455a96fdd5863b1624dbfebf9f84ec04
brendonrodri <rodriguesbrendon861@gmail.com>
2023-02-20 16:22:53 -0300

    Adicionado funcionalide de adicionar tarefa, concluir tarefa, remover tarefa

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react"
+import "./style.css"
+export default function App (){
+  const [task, setTask] = useState([]);
+  const [input, setInput] = useState()
+  const addTask = () =>{
+    if(!input){
+      return
+    }else{
+      setTask([...task, {id: Math.random(), nome: input, done: false }])
+    }
+    setInput("")
+  }
+  const checked = (id) => {
+    const changeDone = task.map((item)=>{
+      if(id === item.id){
+        return{...item, done: !item.done
+        }
+      }
+      return item
+    })
+    setTask(changeDone)
+    console.log(task)
+  }
+  const removeTask = (id)=>{
+    let delTask = task.filter((item)=> item.id !== id)
+    setTask(delTask)
+  }
+  return(
+    <form onSubmit={e=> e.preventDefault()}>
+      <input onChange={e=>setInput(e.target.value)} value={input} />
+      <button onClick={()=>{
+        addTask()
+      }}> Add</button>
+      <ul>
+        {task.map((item)=>(
+          <>
+            <li>{item.nome}</li>
+            <input type="checkbox" onClick={()=>{ checked(item.id)}} />
+            <button onClick={()=>{removeTask(item.id)}}>Del</button>
+          </> 
+        ))}
+      </ul>
+    </form>
+  )
+}
